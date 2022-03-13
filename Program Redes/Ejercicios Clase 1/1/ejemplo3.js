cc4857d20a3db6e838dafa82aced19e2b9c4fe15
MateoGonzalez22 <80712822+MateoGonzalez22@users.noreply.github.com>
2022-03-13 20:04:39 -0300

    Delete Program Redes/Ejercicios Clase 1 directory

--- Program Redes/Ejercicios Clase 1/1/ejemplo3.js
+++
@@ -1,7 +0,0 @@
-// Ejemplo 3
-var variable1 = 'Carlos';
-var variable1 = 'Mara';
-    function print(txt) {
-    console.log(txt)
-}
-print(variable1);
--- Program Redes/Ejercicios Clase 1/4.js
+++
@@ -1,2 +0,0 @@
-var miConvertidor = function (farenheit){return (5/9) * (farenheit - 32)}
-tempCelcius = miConvertidor(70)
