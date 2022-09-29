9b6a72705c707ab3034e0cd93c9811e06368352f
MateoGonzalez22 <alumno22.gonzalez.mateo@ipm.edu.ar>
2022-09-28 21:58:59 -0300

    script terminado

--- Administracion De Redes/sql.sh
+++ Administracion De Redes/sql.sh
@@ -1,43 +1,54 @@
 #!/bin/bash
 
-deleteProcess(){
 
-	PORTS=$(sudo netstat -lgtp)
+# ELIMINAR PROCESO MYSQL (FUE MEJORADO POR FRENAR EL SERVICIO CON SYSTEMCTL)
+# deleteProcess(){
 
-	declare -i index=0
+# 	PORTS=$(sudo netstat -lgtp)
 
-	for i in $PORTS
-	do
+# 	declare -i index=0
 
-		if [ $i == "localhost:mysql" ] 
-		then
-			declare -i index_of_process=$(expr $index + 3)
-		fi
+# 	for i in $PORTS
+# 	do
 
-		if [ "$index" == "$index_of_process" ]
-		then
-			sudo kill ${i:0:-7}
-			echo "Proceso de MYSQL eliminado!"
-		fi
+# 		if [ $i == "localhost:mysql" ] 
+# 		then
+# 			declare -i index_of_process=$(expr $index + 3)
+# 		fi
 
-		index+=1
+# 		if [ "$index" == "$index_of_process" ]
+# 		then
+# 			sudo kill ${i:0:-7}
+# 			echo "Proceso de MYSQL eliminado!"
+# 		fi
 
-	done
+# 		index+=1
+
+# 	done
+# }
+
+stopMySql(){
+
+	sudo systemctl stop mysql
+
 }
 
 deleteContainer(){
 
 	OUTPUT=$(docker run --name mysql -d -e MYSQL_ROOT_PASSWORD=secret -p 3306:3306 mysql 2>&1)
 
+	
+
 	declare -i index=0
 
-	for i in $OUTPUT
+	for x in $OUTPUT
 	do	
-		if [ $index == 16 ]
-		then
-			#echo ${i:1:-2}
-			docker rm -f ${i:1:-2}
-			echo "Volve a ejecutar el script para correr MYSQL"			
+		
+		if [ $index == 16 ]; then
+			
+			#echo ${x:1:-2}
+			docker rm -f ${x:1:-2}
+					
 		fi
 
 		index+=1
@@ -45,43 +56,64 @@ deleteContainer(){
 }
 
 
+checkResponse(){
 
-OUTPUT=$(docker run --name mysql -d -e MYSQL_ROOT_PASSWORD=secret -p 3306:3306 mysql 2>&1)
-substring="docker: Error response from daemon: Conflict."
+	OUTPUT=$(docker run --name mysql -d -e MYSQL_ROOT_PASSWORD=secret -p 3306:3306 mysql 2>&1)
+	already_in_use="docker: Error response from daemon: Conflict. The container name"
+	port_being_used="Error response from daemon: driver failed programming external connectivity on endpoint"
 
-x=""
-
-
-case $OUTPUT in
-
-  *"docker: Error response from daemon: Conflict."*)
-	echo "Eliminando contenedor..."
-	x="container"
-    ;;
-esac
-
-
-
-
-if [ "$x" == "container" ]
-then
-	deleteContainer
-else
 	len=${#OUTPUT}
 
-	if [ "$len" == "64" ]
-	then
-		echo "Mysql Corriendo en el contenedor ${OUTPUT}"
+	if [[ $OUTPUT =~ .*$already_in_use.* ]]; then
+		
+		return 1
+		
+	elif [[ $OUTPUT =~ .*$port_being_used.* ]]; then
+		
+		return 2
+		
+	elif [ "$len" == "64" ]; then
+		echo -e "\nMysql Corriendo en el contenedor ${OUTPUT}"
+		return 200
 	else
-		echo "Error desconocido"
+		echo $OUTPUT
+		return 404
 	fi
+
+}
+
+
+
+run(){
+
+	declare -i i=0
+
+	while [ $i == 0 ]
+	do
+
+		checkResponse
+		response=$?
+
+		if [[ $response == 1 ]]; then
+			echo -e "\nEliminando contenedor antiguo..."
+			deleteContainer
+		elif [[ $response == 2 ]]; then
+			echo -e "\nFrenando servicio MySQL..."
+			stopMySql
+		elif [[ $response == 200 ]]; then
+			i=1
+		elif [[ $response == 404 ]]; then
+			echo -e "\nError desconocido!"
+		fi
+
+	done
+
 	
-fi
+		
 
+}	
 
-
-
-
+run
 
 
 
